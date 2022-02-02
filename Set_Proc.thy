5a0871b37dccadcb8a451030286052dd516d6379
Lukas Stevens <mail@lukas-stevens.de>
2022-02-02 22:44:41 +0100

    Continue

--- Set_Proc.thy
+++ Set_Proc.thy
@@ -1,6 +1,8 @@
 theory Set_Proc                
-  imports Main Logic ZFC_in_HOL.ZFC_in_HOL
+  imports Main Logic ZFC_in_HOL.ZFC_in_HOL Graph_Theory.Graph_Theory
 begin
+
+abbreviation "vset \<equiv> ZFC_in_HOL.set"
 
 hide_const (open) ZFC_in_HOL.set
 
@@ -9,7 +11,7 @@ datatype 'a pset_term =
   Union "'a pset_term" "'a pset_term" (infixr \<open>\<squnion>\<^sub>s\<close> 50) |
   Inter "'a pset_term" "'a pset_term" (infixr \<open>\<sqinter>\<^sub>s\<close> 60) |
   Diff "'a pset_term" "'a pset_term"  (infixl \<open>-\<^sub>s\<close> 70) |
-  Set "'a pset_term" "'a pset_term list"
+  Single "'a pset_term"
 
 datatype 'a pset_atom =
   Elem "'a pset_term" "'a pset_term" (infix \<open>\<in>\<^sub>s\<close> 45) | 
@@ -18,7 +20,7 @@ datatype 'a pset_atom =
 
 type_synonym 'a pset_literal = "bool \<times> 'a pset_atom"
 
-definition "vdiff s1 s2 \<equiv> ZFC_in_HOL.set (elts s1 - elts s2)"
+definition "vdiff s1 s2 \<equiv> vset (elts s1 - elts s2)"
 
 fun I\<^sub>s\<^sub>t :: "('a \<Rightarrow> V) \<Rightarrow> 'a pset_term \<Rightarrow> V" where
   "I\<^sub>s\<^sub>t v \<emptyset> = 0"
@@ -26,7 +28,7 @@ fun I\<^sub>s\<^sub>t :: "('a \<Rightarrow> V) \<Rightarrow> 'a pset_term \<Righ
 | "I\<^sub>s\<^sub>t v (s1 \<squnion>\<^sub>s s2) = I\<^sub>s\<^sub>t v s1 \<squnion> I\<^sub>s\<^sub>t v s2"
 | "I\<^sub>s\<^sub>t v (s1 \<sqinter>\<^sub>s s2) = I\<^sub>s\<^sub>t v s1 \<sqinter> I\<^sub>s\<^sub>t v s2"
 | "I\<^sub>s\<^sub>t v (s1 -\<^sub>s s2) = vdiff (I\<^sub>s\<^sub>t v s1) (I\<^sub>s\<^sub>t v s2)"
-| "I\<^sub>s\<^sub>t v (Set s1 ss) = fold vinsert (map (I\<^sub>s\<^sub>t v) (s1 # ss)) 0"
+| "I\<^sub>s\<^sub>t v (Single s) = vset {I\<^sub>s\<^sub>t v s}"
 
 fun I\<^sub>s\<^sub>a :: "('a \<Rightarrow> V) \<Rightarrow> 'a pset_atom \<Rightarrow> bool" where
   "I\<^sub>s\<^sub>a v (t1 \<in>\<^sub>s t2) \<longleftrightarrow> I\<^sub>s\<^sub>t v t1 \<in> elts (I\<^sub>s\<^sub>t v t2)"
@@ -37,6 +39,279 @@ definition I\<^sub>s\<^sub>l :: "('a \<Rightarrow> V) \<Rightarrow> 'a pset_lite
   "I\<^sub>s\<^sub>l v \<equiv> (\<lambda>(b, a). b \<longleftrightarrow> I\<^sub>s\<^sub>a v a)"
 
 type_synonym 'a pset_fm = "'a pset_literal fm"
+
+abbreviation ancestors1 :: "('a,'b) pre_digraph \<Rightarrow> 'a \<Rightarrow> 'a set"
+  where "ancestors1 G s \<equiv> {u. u \<rightarrow>\<^bsub>G\<^esub> s}"
+
+abbreviation ancestors :: "('a,'b) pre_digraph \<Rightarrow> 'a \<Rightarrow> 'a set"
+  where "ancestors G s \<equiv> {u. u \<rightarrow>\<^sup>+\<^bsub>G\<^esub> s}"
+
+lemma (in fin_digraph) finite_ancestors1[intro]: "finite (ancestors1 G s)"
+  using reachable_in_verts
+  by (auto intro: rev_finite_subset[where ?A="ancestors1 G s", OF finite_verts])
+
+lemma (in fin_digraph) small_ancestors1[intro]: "small (ancestors1 G s)"
+  using finite_imp_small finite_ancestors1 by blast
+
+lemma (in fin_digraph) finite_ancestors[intro]: "finite (ancestors G s)"
+  using reachable_in_verts
+  by (auto intro: rev_finite_subset[where ?A="ancestors G s", OF finite_verts])
+
+lemma (in fin_digraph) small_ancestors[intro]: "small (ancestors G s)"
+  using finite_imp_small finite_ancestors by blast
+
+lemma (in wf_digraph) in_ancestors_if_dominates[simp, intro]:
+  assumes "s \<rightarrow>\<^bsub>G\<^esub> t"
+  shows "s \<in> ancestors G t"
+  using assms by blast
+
+lemma (in wf_digraph) ancestors_mono:
+  assumes "s \<in> ancestors G t"
+  shows "ancestors G s \<subseteq> ancestors G t"
+  using assms by fastforce
+
+locale dag = digraph G for G +
+  assumes acyclic: "\<nexists>c. cycle c"
+begin
+
+lemma ancestors_not_comm:
+  assumes "s \<in> ancestors G t"
+  shows "t \<notin> ancestors G s"
+proof
+  assume "t \<in> ancestors G s"
+  then obtain p1 p2 where "awalk t p1 s" "p1 \<noteq> []" "awalk s p2 t" "p2 \<noteq> []"
+    using assms reachable1_awalk by auto
+  then have "closed_w (p1 @ p2)"
+    unfolding closed_w_def by auto
+  with closed_w_imp_cycle acyclic show False
+    by blast
+qed
+
+lemma ancestors_strict_mono:
+  assumes "s \<in> ancestors G t"
+  shows "ancestors G s \<subset> ancestors G t"
+  using assms ancestors_mono ancestors_not_comm by blast
+
+lemma card_ancestors_strict_mono:
+  assumes "s \<rightarrow>\<^bsub>G\<^esub> t"
+  shows "card (ancestors G s) < card (ancestors G t)"
+  using assms finite_ancestors ancestors_strict_mono
+  by (metis in_ancestors_if_dominates psubset_card_mono)
+
+end
+
+locale realization = dag G for G +
+  fixes P T :: "'a set"
+  fixes I :: "'a \<Rightarrow> V"
+  assumes P_T_partition_verts: "P \<inter> T = {}" "verts G = P \<union> T"
+begin
+
+function height :: "'a \<Rightarrow> nat" where
+  "t \<in> P \<Longrightarrow> height t = 0"
+| "t \<notin> P \<Longrightarrow> \<forall>s. \<not> s \<rightarrow>\<^bsub>G\<^esub> t \<Longrightarrow> height t = 0"
+| "t \<notin> P \<Longrightarrow> s \<rightarrow>\<^bsub>G\<^esub> t \<Longrightarrow> height t = Max (height ` ancestors1 G t) + 1"
+  by auto
+termination
+  by (relation "measure (\<lambda>t. card (ancestors G t))") (simp_all add: card_ancestors_strict_mono)
+
+lemma height_cases':
+  obtains (P_0) "t \<in> P" "height t = 0"
+    | (nP_0) "t \<notin> P" "\<forall>s. \<not> s \<rightarrow>\<^bsub>G\<^esub> t" "height t = 0"
+    | (nP_Suc) s where "t \<notin> P" "s \<rightarrow>\<^bsub>G\<^esub> t" "height t = height s + 1"
+proof(cases t rule: height.cases)
+  case (3 t s)
+  note Max_in[OF finite_imageI[where ?h=height, OF finite_ancestors1]]
+  with 3 that show ?thesis
+    by auto
+qed simp_all
+
+function realization :: "'a \<Rightarrow> V" where
+  "x \<in> P \<Longrightarrow> realization x = vset {I x}"
+| "t \<in> T \<Longrightarrow> realization t = vset (realization ` ancestors1 G t)"
+| "x \<notin> P \<union> T \<Longrightarrow> realization x = 0"
+  using P_T_partition_verts by auto
+termination
+  by (relation "measure (\<lambda>t. card (ancestors G t))") (simp_all add: card_ancestors_strict_mono)
+
+lemma lemma1_1:
+  assumes "s \<in> P \<union> T" "t \<in> T" "s \<rightarrow>\<^bsub>G\<^esub> t"
+  shows "height s < height t"
+  using assms
+proof(cases t rule: height.cases)
+  case (3 t u)
+  note Max_ge[OF finite_imageI[where ?h=height, OF finite_ancestors1], of "height s" t]
+  with assms 3 show ?thesis
+    by auto
+qed (use P_T_partition_verts in auto)
+
+lemma dominates_if_mem_realization:
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<union> T \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> realization y"
+  assumes "s \<in> P \<union> T" "t \<in> P \<union> T"
+  assumes "realization s \<in> elts (realization t)"
+  obtains s' where "s' \<rightarrow>\<^bsub>G\<^esub> t" "realization s = realization s'"
+  using assms(2-)
+proof(induction t rule: realization.induct)
+  case (1 x)
+  with assms(1)[OF \<open>x \<in> P\<close>] show ?case 
+    apply(simp)
+    by (metis "1.prems"(4) mem_not_sym)
+next
+  case (2 t)
+  then show ?case
+    apply(auto)
+     apply (metis (no_types, lifting) emptyE image_iff mem_Collect_eq)+
+    done
+next
+  case (3 x)
+  then show ?case by simp
+qed
+
+lemma lemma1_2':
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> I y"
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<union> T \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> realization y"
+  assumes "t1 \<in> P \<union> T" "t2 \<in> P \<union> T" "realization t1 = realization t2"
+  shows "height t1 \<le> height t2"
+proof -
+  from assms(3,4) consider "t1 \<in> P" | "t1 \<in> T" "t2 \<in> P" | "t1 \<in> T" "t2 \<in> T"
+    using P_T_partition_verts by blast
+  then show ?thesis
+  proof cases
+    case 1
+    with assms show ?thesis by auto
+  next
+    case 2
+    then show ?thesis
+    proof(cases t1 rule: height.cases)
+      case (3 t s)
+      have "small (realization ` ancestors1 G t1)"
+        by blast+
+      with 2 3 assms(5) have "realization ` ancestors1 G t1 = {I t2}"
+        using \<open>small (realization ` ancestors1 G t1)\<close> by force
+      moreover from 3 adj_in_verts P_T_partition_verts have "s \<in> P \<union> T"
+        by simp
+      then have "I t2 \<noteq> realization s"
+        using 2 3 assms(2,3,5) by metis
+      ultimately show ?thesis
+        using 3 by (metis ex_in_conv imageI insert_iff mem_Collect_eq)
+    qed auto
+  next
+    case 3
+    with P_T_partition_verts have "t1 \<in> T" "t2 \<in> T"
+      by simp_all
+    then show ?thesis
+      using assms(5)
+    proof(induction "height t2" arbitrary: t1 t2 rule: less_induct)
+      case less
+      then show ?case
+      proof(cases "height t2")
+        case 0
+        have "small (realization ` ancestors1 G t)" for t
+          using small_ancestors1 by simp
+        with 0 less.prems assms(2) show ?thesis
+          apply(cases t2 rule: height_cases')
+            apply(auto)
+           apply (metis Int_iff P_T_partition_verts(1) empty_iff)
+          by (metis (no_types, lifting) Collect_empty_eq elts_of_set empty_is_image height.simps(1,2)
+              less.prems(3) realization.simps(2))
+      next
+        case (Suc x)
+        then have "t2 \<notin> P"
+          using less.prems(2) by fastforce
+        then show ?thesis
+        proof(cases t1 rule: height_cases')
+          case (nP_Suc s)
+          with P_T_partition_verts adj_in_verts(1) have "s \<in> P \<union> T"
+            by blast
+          from nP_Suc less.prems(1) have "realization s \<in> elts (realization t1)"
+            by auto
+          then obtain s' where s': "realization s' = realization s" "s' \<rightarrow>\<^bsub>G\<^esub> t2"
+            using dominates_if_mem_realization \<open>s \<in> P \<union> T\<close> less.prems assms(2)
+            by (metis Un_iff)
+          then have "s' \<in> P \<union> T"
+            using P_T_partition_verts(2) adj_in_verts(1) by blast
+
+          note lemma1_1[OF this(1) less.prems(2) \<open>s' \<rightarrow>\<^bsub>G\<^esub> t2\<close>]
+          from less(1)[OF this _ _ s'(1)[symmetric]] have "height s \<le> height s'" if "s \<in> T" "s' \<in> T"
+            using that P_T_partition_verts(2) adj_in_verts(1) nP_Suc(2) s'(2) by blast
+          with nP_Suc have ?thesis if "s \<in> T" "s' \<in> T"
+            using that \<open>height s' < height t2\<close> by linarith
+
+          moreover have "height s \<le> height s'" if "s \<in> T" "s' \<notin> T"
+          proof -
+            from that \<open>s' \<in> P \<union> T\<close> P_T_partition_verts have "s' \<in> P"
+              by simp
+            with that show ?thesis
+            proof(cases s rule: height.cases)
+              case (3 _ u)
+              then have "u \<in> P \<union> T"
+                using P_T_partition_verts(2) adj_in_verts(1) by blast
+              with 3 that have "realization u \<in> elts (realization s)"
+                by auto
+              then obtain u' where "u' \<rightarrow>\<^bsub>G\<^esub> s'" "realization u' = realization u"
+                using dominates_if_mem_realization
+                by (metis \<open>s' \<in> P \<union> T\<close> \<open>u \<in> P \<union> T\<close> assms(2) s'(1))
+
+              then show ?thesis
+                using \<open>realization u \<in> elts (realization s)\<close> \<open>s' \<in> P\<close> assms(2) s'(1)
+                by (metis P_T_partition_verts(2)
+                    adj_in_verts(1) elts_of_set mem_not_refl realization.simps(1)
+                    singletonD small_empty small_insert)
+            qed auto
+          qed
+          with nP_Suc have ?thesis if "s \<in> T" "s' \<notin> T"
+
+            using that \<open>height s' < height t2\<close> by linarith
+          ultimately show ?thesis
+            using nP_Suc Suc \<open>s \<in> P \<union> T\<close> by auto
+        qed auto
+      qed
+    qed
+  qed
+qed
+
+lemma lemma1_2:
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> I y"
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<union> T \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> realization y"
+  assumes "t1 \<in> P \<union> T" "t2 \<in> P \<union> T" "realization t1 = realization t2"
+  shows "height t1 = height t2"
+  using assms lemma1_2' le_antisym by metis
+    
+lemma lemma1_3:
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> I y"
+  assumes "\<And>x y. x \<in> P \<Longrightarrow> y \<in> P \<union> T \<Longrightarrow> x \<noteq> y \<Longrightarrow> I x \<noteq> realization y"
+  assumes "s \<in> P \<union> T" "t \<in> P \<union> T" "realization s \<in> elts (realization t)"
+  shows "height s < height t"
+proof -
+  from assms dominates_if_mem_realization obtain s' where
+    s': "realization s' = realization s" "s' \<rightarrow>\<^bsub>G\<^esub> t" by metis
+  then have "s' \<in> P \<union> T"
+    using adj_in_verts P_T_partition_verts by blast
+  from assms(2-5) have "t \<in> T"
+    by (metis elts_of_set mem_not_sym realization.cases realization.simps(1) singletonD
+              small_empty small_insert)
+  with lemma1_1[OF \<open>s' \<in> P \<union> T\<close>] assms s' have "height s' < height t"
+    by auto
+  moreover from lemma1_2[OF _ _ \<open>s' \<in> P \<union> T\<close> _ s'(1)] have "height s' = height s"
+    using assms by blast
+  ultimately show ?thesis
+    by linarith
+qed
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 type_synonym 'a branch = "'a pset_fm list"
 
 definition vars :: "'a branch \<Rightarrow> 'a set" where
@@ -53,21 +328,6 @@ fun member_cycle :: "'a pset_literal list \<Rightarrow> bool" where
 
 abbreviation "AT a \<equiv> Atom (True, a)"
 abbreviation "AF a \<equiv> Atom (False, a)"
-
-fun tlvl_terms_term :: "'a pset_term \<Rightarrow> 'a pset_term set" where
-  "tlvl_terms_term (Set t0 ts) = insert t0 (set ts)"
-| "tlvl_terms_term x = {x}"
-
-fun tlvl_terms_atom :: "'a pset_atom \<Rightarrow> 'a pset_term set" where
-  "tlvl_terms_atom (s \<in>\<^sub>s t) = tlvl_terms_term s \<union> tlvl_terms_term t"
-| "tlvl_terms_atom (s \<approx>\<^sub>s t) = tlvl_terms_term s \<union> tlvl_terms_term t"
-| "tlvl_terms_atom (s \<sqsubseteq>\<^sub>s t) = tlvl_terms_term s \<union> tlvl_terms_term t"
-
-fun tlvl_terms_lit :: "'a pset_literal \<Rightarrow> 'a pset_term set" where
-  "tlvl_terms_lit (_, a) = tlvl_terms_atom a"
-
-definition tlvl_terms :: "'a branch \<Rightarrow> 'a pset_term set" where
-  "tlvl_terms b \<equiv> \<Union>(tlvl_terms_lit ` Atoms (set b))"
 
 inductive closeable :: "'a branch \<Rightarrow> bool" where
   Or: "\<lbrakk> Or \<phi>\<^sub>1 \<phi>\<^sub>2 \<in> set branch; closeable (\<phi>\<^sub>1 # branch); closeable (\<phi>\<^sub>2 # branch) \<rbrakk>
